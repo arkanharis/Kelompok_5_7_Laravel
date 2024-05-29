9071e99bbe36049edd6e2eb37cf5d1f298c858ea
Alhusna Hanifah <alhusnahanifah@gmail.com>
2024-05-30 04:25:38 +0700

    merge dan  inisialisasi

---
+++ storage/framework/views/d03ca148eddfd2e006750399e99a50ec.php
@@ -0,0 +1,20 @@
+<header class="mn-header navbar-fixed">
+    <nav class="cyan darken-1">
+        <div class="nav-wrapper row">
+            <section class="material-design-hamburger navigation-toggle">
+                <a href="#" data-activates="slide-out"
+                    class="button-collapse show-on-large material-design-hamburger__icon">
+                    <span class="material-design-hamburger__layer"></span>
+                </a>
+            </section>
+            <div class="header-title col s3">
+                <span class="chapter-title">ELMS | Employee Leave Management System</span>
+            </div>
+
+
+            </form>
+
+
+        </div>
+    </nav>
+</header><?php /**PATH C:\Users\ASUS TUF15\Documents\XAMPP\htdocs\Kelompok_5_7_Laravel\resources\views/partials/nav.blade.php ENDPATH**/ ?>
