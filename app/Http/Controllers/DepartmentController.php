c5036901556781ebe24d70e1e3887c621ff27662
Farah Nasywa <166036176+farahNasywa@users.noreply.github.com>
2024-05-30 03:54:31 +0700

    Add files via upload

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -2,65 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Department;
-use App\Http\Requests\StoreDepartmentRequest;
-use App\Http\Requests\UpdateDepartmentRequest;
 
 class DepartmentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('add_department');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreDepartmentRequest $request)
+    public function store(Request $request)
     {
-        //
-    }
+        $request->validate([
+            'department_name' => 'required',
+            'department_short_name' => 'required',
+            'department_code' => 'required',
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Department $department)
-    {
-        //
-    }
+        $department = new Department();
+        $department->name = $request->department_name;
+        $department->short_name = $request->department_short_name;
+        $department->code = $request->department_code;
+        $department->save();
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Department $department)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateDepartmentRequest $request, Department $department)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Department $department)
-    {
-        //
+        return redirect()->route('department.create')->with('success', 'Department added successfully');
     }
 }
